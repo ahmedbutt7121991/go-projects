049d8a5eb2062ee74c480c948f6755b3dbcdfbe6
Ahmed butt <Ahmed.butt91@gmail.com>
2022-05-21 01:36:22 +0000

    add data

--- go-crud/main.go
+++ go-crud/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/gotilla/mux"
@@ -24,6 +26,9 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 
+	movies = append(movies, Movie{ID: "1", Isbn: "00001", Title: "Harry Potter 01", Director: &Director{Firstname: "JK", Lastname: "Rolling"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "00002", Title: "Harry Potter 02", Director: &Director{Firstname: "JK", Lastname: "Rolling"}})
+
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
@@ -31,4 +36,5 @@ func main() {
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Server Listening on Port 8000...\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
