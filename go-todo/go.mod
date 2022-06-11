9a39733087bc9af4ef978f84852ea524ff15cdc8
Ahmed butt <Ahmed.butt91@gmail.com>
2022-06-11 19:58:47 +0000

    go-todo added

---
+++ go-todo/go.mod
@@ -0,0 +1,3 @@
+module github.com/ahmedbutt7121991/go-project/go-todo
+
+go 1.18
---
+++ go-todo/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"log"
+)
+
+var tmpl *template.Template
+
+type Todo struct {
+	Item string
+	Done bool
+}
+
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+
+
+func todo(w http.ResponseWriter, r *http.Request) {
+	data := PageData {
+		Title: "TODO LIST",
+		Todos: []Todo{
+			{Item: "Install Go", Done: true},
+			{Item: "Learn Go", Done: false},
+			{Item: "Make Todo List in Go", Done: false},
+		},
+	}
+
+	tmpl.Execute(w, data)
+}
+
+
+func main() {
+	mux := http.NewServeMux()
+
+	tmpl  = template.Must(template.ParseFiles("templates/index.gohtml"))
+
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/todo", todo)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:9090", mux))
+}
